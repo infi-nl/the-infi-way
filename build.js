0583b6f2ea0d68befb10115ab37739932f8d9dbc
Luca Scalzotto <luca@scorpiac.com>
2022-03-02 18:45:28 +0100

    Remove build.sh, move logic to build.js, move hardcoded paths to const

--- build.js
+++ build.js
@@ -1,38 +1,39 @@
 #!/usr/bin/env node
 const fs = require('fs/promises');
 
+const templateFile = `${__dirname}/template.html`;
+const contentDir = `${__dirname}/content`;
+const outputDir = `${__dirname}/build`;
+
 (async () => {
-  const template = (await fs.readFile(`${__dirname}/template.html`)).toString();
-  const langFiles = await fs.readdir(`${__dirname}/content`);
-  await Promise.all(
-    langFiles
-      .map((f) => `${__dirname}/content/${f}`)
-      .map((f) => processTemplate(template, f)),
-  );
+  const template = (await fs.readFile(templateFile)).toString();
+  const contentFiles = await fs.readdir(contentDir);
+  await fs.mkdir(outputDir, { recursive: true });
+
+  for (const f of contentFiles) {
+    const lang = f.substring(f.lastIndexOf('/') + 1, f.lastIndexOf('.'));
+    console.log(`Processing language: ${lang}`);
+    const content = JSON.parse((await fs.readFile(`${contentDir}/${f}`)).toString());
+    content.lang = lang;
+    const processed = processTemplate(template, content);
+    await fs.writeFile(`${outputDir}/${lang}.html`, processed);
+  }
+
   console.log('Done!');
 })();
 
-async function processTemplate(template, langFile) {
-  const lang = langFile.substring(langFile.lastIndexOf('/') + 1, langFile.lastIndexOf('.'));
-  console.log(`Processing language: ${lang}`);
-  const content = JSON.parse((await fs.readFile(langFile)).toString());
-  content.lang = lang;
-  const processed = processL10n(template, content);
-  await fs.writeFile(`${__dirname}/build/${lang}.html`, processed);
-}
-
-function processL10n(template, content) {
+function processTemplate(template, content) {
   if (typeof content === 'object') {
-    return processL10nObject(template, content);
+    return processTemplateObject(template, content);
   }
   if (typeof content === 'string') {
-    return processL10nString(template, content);
+    return processTemplateString(template, content);
   }
   console.error(`Unknown localization content type: ${typeof content}`);
   return template;
 }
 
-function processL10nObject(template, content, prefix = '') {
+function processTemplateObject(template, content, prefix = '') {
   let result = template;
 
   Object.entries(content).forEach(([key, val]) => {
@@ -48,12 +49,12 @@ function processL10nObject(template, content, prefix = '') {
       while (eachStartIndex >= 0) {
         const eachEndIndex = result.indexOf(eachEndToken, eachStartIndex + eachStartToken.length);
         const subTemplate = result.substring(eachStartIndex + eachStartToken.length, eachEndIndex);
-        const subProcessed = val.map((v) => processL10n(subTemplate, v)).join('');
+        const subProcessed = val.map((v) => processTemplate(subTemplate, v)).join('');
         result = spliceString(result, eachStartIndex, eachEndIndex + eachEndToken.length, subProcessed);
         eachStartIndex = result.indexOf(eachStartToken);
       }
     } else if (typeof val === 'object') {
-      result = processL10nObject(result, val, `${fullKey}.`);
+      result = processTemplateObject(result, val, `${fullKey}.`);
     } else {
       console.error(`Unknown localized value: ${val}`);
     }
@@ -62,7 +63,7 @@ function processL10nObject(template, content, prefix = '') {
   return result;
 }
 
-function processL10nString(template, content) {
+function processTemplateString(template, content) {
   return template.replaceAll('{{this}}', content);
 }
 
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-mkdir -p build
-./process_template.js
