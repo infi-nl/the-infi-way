87410a53a19227fd03d3ad848c1f0dd0f5ca3a37
Luca Scalzotto <luca@scorpiac.com>
2023-10-14 10:30:16 +0200

    Working csp generation, update hash

--- generate-csp.sh
+++ generate-csp.sh
@@ -13,5 +13,12 @@ then
   exit 1
 fi
 
+# Get the text from the script tag.
 scriptContent=$(htmlq -f 'src/template.html' -t 'script')
+# Calculate the new hash.
 hash=$(echo -n "$scriptContent" | openssl sha256 -binary | openssl base64)
+# Replace the old hash in the CSP with the new hash.
+# Note that we use '|' as the sed delimiter here, because '/' can be part of the hash.
+newHtml=$(sed -E "s|^(.*sha256-).*('.*)$|\1$hash\2|" 'src/template.html')
+# Save the newly generated file.
+echo "$newHtml" > 'src/template.html'
